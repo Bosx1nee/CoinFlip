03c6903618cd1e618109cf6ce213836fc22a0321
Your Name <you@example.com>
2018-10-03 14:35:22 -0600

    commit

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace coinflip
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int coin;
+            string userGuess;
+            Random rng = new Random();
+
+            Console.Write("Enter Guess Heads ot Tails (H or T)");
+            userGuess = Console.ReadLine();
+
+            coin = rng.Next(0, 2);
+
+            if(coin == 0 && userGuess == "T")
+            {
+                Console.WriteLine("The coin was Tails, you win!");
+            }
+            else if (coin == 1 && userGuess == "H")
+            {
+                if (coin == 0)
+                {
+                    Console.WriteLine("The coin was Tails, you lose");
+                }
+                else
+                {
+                    Console.WriteLine();
+                }
+            }
+        }
+    }
+}
